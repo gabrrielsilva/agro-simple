0a1b4aff75aaf37a3ef437f342eec777ce7b7a63
gabrrielsilva <gabrielsilvaev@gmail.com>
2022-12-07 13:36:24 -0300

    created: rest api and query

--- app/infra/database.ts
+++ app/infra/database.ts
@@ -1,6 +1,6 @@
 import { config } from "https://deno.land/std@0.163.0/dotenv/mod.ts";
-import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
+import postgres from 'https://deno.land/x/postgresjs@v3.3.2/mod.js';
 
 const envVars = await config();
 
-export const client = new Client(envVars.DATABASE_URL);
+export const sql = postgres(envVars.DATABASE_URL);
--- app/main.ts
+++ app/main.ts
@@ -1,15 +1,30 @@
-import { startStandaloneServer } from "npm:@apollo/server@4.1/standalone";
-import { ApolloServer } from "npm:@apollo/server@^4.1";
-import { resolvers } from "./resolvers.ts";
-import { typeDefs } from "./schema.ts";
+import { serve } from "https://deno.land/std@0.167.0/http/server.ts";
+import { sql } from './infra/database.ts';
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+serve(handler, { port: 3000 });
+console.log("Server is running");
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 8000 },
-});
+async function handler(req: Request): Promise<Response> {
+  const url = new URL(req.url).pathname;
+  const method = req.method;
+  const body = JSON.parse(await req.text());
 
-console.log(`Server running on: ${url}`)
+  if (url === "/" && method === "POST") {
+    const { cultura, regiao } = body;
+    const [calendarioCultivo] = await sql`SELECT id, epoca_plantio, dias_cultivo FROM agro.cultura_regiao cr INNER JOIN agro.cultura c ON c.nome = cr.cultura WHERE cr.cultura = ${cultura} AND regiao = ${regiao}`;
+
+    return new Response(JSON.stringify(calendarioCultivo), {
+      status: 200,
+      headers: {
+        "content-type": "application/json; charset=utf-8",
+      },
+    });
+  }
+
+  return new Response(JSON.stringify({ message: "NOT FOUND" }), {
+    status: 404,
+    headers: {
+      "content-type": "application/json; charset=utf-8",
+    },
+  });
+}
